4974356fbf0a6500527e9c9875d7181bdbf1bd06
Maks <usikmaks3@gmail.com>
2024-11-27 12:46:35 +0100

    dsf

--- shadow-labyrinth/Sandbox/BasicGame/src/BasicGame.java
+++ shadow-labyrinth/Sandbox/BasicGame/src/BasicGame.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 
 public class BasicGame implements GameLoop {
     ArrayList<Map> tiles = new ArrayList<>();
+
     public static void main(String[] args) {
         SaxionApp.startGameLoop(new BasicGame(), 768, 576, 40);
     }
@@ -36,11 +37,12 @@ public class BasicGame implements GameLoop {
 
     public void loadMap() {
         Map grass = new Map();
-        grass.image = "Shadow-Labyrinth\\shadow-labyrinth\\Sandbox\\resources\\images\\map\\grass00.png";
+        grass.image = "shadow-labyrinth/Sandbox/resources/images/map/grass00.png";
 
         tiles.add(grass);
     }
-    public void drawMap () {
+
+    public void drawMap() {
         SaxionApp.drawImage(tiles.get(0).image, 100, 100);
     }
 
