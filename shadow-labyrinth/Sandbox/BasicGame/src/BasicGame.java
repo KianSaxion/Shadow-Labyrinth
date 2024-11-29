0b5d7a570cb4ec0b6b0e7788bf1f6b13a910da22
Kian <159238818+KianKamphuis@users.noreply.github.com>
2024-11-29 10:11:27 +0100

    player demo for testing purposes

--- shadow-labyrinth/Sandbox/BasicGame/src/BasicGame.java
+++ shadow-labyrinth/Sandbox/BasicGame/src/BasicGame.java
@@ -3,6 +3,7 @@ import nl.saxion.app.SaxionApp;
 import nl.saxion.app.interaction.GameLoop;
 import nl.saxion.app.interaction.KeyboardEvent;
 import nl.saxion.app.interaction.MouseEvent;
+import java.awt.event.KeyEvent;
 
 import java.util.ArrayList;
 
@@ -10,6 +11,7 @@ public class BasicGame implements GameLoop {
     ArrayList<Map> tiles = new ArrayList<>();
     final int MAX_SCREEN_COL = 16;
     final int MAX_SCREEN_ROW = 16;
+    Cookiemonster cookiemonster;
 
     public static void main(String[] args) {
         SaxionApp.startGameLoop(new BasicGame(), 768, 576, 40);
@@ -17,6 +19,9 @@ public class BasicGame implements GameLoop {
 
     @Override
     public void init() {
+        cookiemonster = new Cookiemonster();
+        cookiemonster.x = 100;
+        cookiemonster.y = 100;
     }
 
 
@@ -24,11 +29,32 @@ public class BasicGame implements GameLoop {
     public void loop() {
         loadMap();
         drawMap();
+        SaxionApp.drawImage(cookiemonster.imageFile, cookiemonster.x, cookiemonster.y, 32, 32);
+        cookiemonster.x += cookiemonster.xSpeed;
+        cookiemonster.y += cookiemonster.ySpeed;
     }
 
     @Override
     public void keyboardEvent(KeyboardEvent keyboardEvent) {
-
+        if (keyboardEvent.isKeyPressed()) {
+            if (keyboardEvent.getKeyCode() == KeyEvent.VK_LEFT) {
+                if (cookiemonster.xSpeed > 0) {
+                    cookiemonster.xSpeed = 0;
+                } else {
+                    cookiemonster.xSpeed -= 1;
+                }
+            } else if (keyboardEvent.getKeyCode() == KeyEvent.VK_RIGHT) {
+                if(cookiemonster.xSpeed < 0) {
+                    cookiemonster.xSpeed = 0;
+                } else {
+                    cookiemonster.xSpeed += 1;
+                }
+            } else if (keyboardEvent.getKeyCode() == KeyEvent.VK_UP) {
+                cookiemonster.ySpeed -= 1;
+            } else if (keyboardEvent.getKeyCode() == KeyEvent.VK_DOWN) {
+                cookiemonster.ySpeed += 1;
+            }
+        }
     }
 
     @Override
---
+++ shadow-labyrinth/Sandbox/BasicGame/src/Cookiemonster.java
@@ -0,0 +1,9 @@
+public class Cookiemonster {
+        int x;
+        int y;
+
+        int xSpeed;
+        int ySpeed;
+
+        String imageFile = "shadow-labyrinth/Sandbox/resources/images/player/cookie.jpg";
+}
