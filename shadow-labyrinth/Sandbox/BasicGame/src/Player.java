7587d233cc97b5ae4a95eb5f04cfd105715e6e62
Maks <usikmaks3@gmail.com>
2024-12-10 19:46:23 +0100

    restructured the project

--- shadow-labyrinth/Sandbox/BasicGame/src/Player.java
+++ shadow-labyrinth/Sandbox/BasicGame/src/Player.java
@@ -2,8 +2,8 @@ import nl.saxion.app.SaxionApp;
 
 public class Player {
     int worldX, worldY;
-    int screenX = SaxionApp.getWidth() / 2; // Center of the screen
-    int screenY = SaxionApp.getHeight() / 2; // Center of the screen
+    int screenX = 768 / 2; // Center of the screen
+    int screenY = 576 / 2; // Center of the screen
     int xSpeed, ySpeed;
     String imageFile = "shadow-labyrinth/Sandbox/resources/images/player/MCfront.png";
 
