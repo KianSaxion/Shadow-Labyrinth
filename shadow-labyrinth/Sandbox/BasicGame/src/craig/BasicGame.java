648b42a1fda311e6cb1e4b432bccbd9926d81b1e
Kian <159238818+KianKamphuis@users.noreply.github.com>
2024-12-11 12:02:36 +0100

    bug fix

--- shadow-labyrinth/Sandbox/BasicGame/src/craig/BasicGame.java
+++ shadow-labyrinth/Sandbox/BasicGame/src/craig/BasicGame.java
@@ -1,42 +1,42 @@
-package craig;
-
-import nl.saxion.app.SaxionApp;
-
-import nl.saxion.app.interaction.GameLoop;
-import nl.saxion.app.interaction.KeyboardEvent;
-import nl.saxion.app.interaction.MouseEvent;
-
-public class BasicGame implements GameLoop {
-    Map map = new Map();
-
-    public static void main(String[] args) {
-        SaxionApp.startGameLoop(new BasicGame(), 1000, 1000, 40);
-    }
-
-    @Override
-    public void init() {
-
-    }
-
-    @Override
-    public void loop() {
-        SaxionApp.clear();
-        map.draw();
-    }
-
-    @Override
-    public void keyboardEvent(KeyboardEvent keyboardEvent) {
-
-    }
-
-    @Override
-    public void mouseEvent(MouseEvent mouseEvent) {
-
-    }
-}
-
-
-
-
-
-
+//package craig;
+//
+//import nl.saxion.app.SaxionApp;
+//
+//import nl.saxion.app.interaction.GameLoop;
+//import nl.saxion.app.interaction.KeyboardEvent;
+//import nl.saxion.app.interaction.MouseEvent;
+//
+//public class BasicGame implements GameLoop {
+//    Map map = new Map();
+//
+//    public static void main(String[] args) {
+//        SaxionApp.startGameLoop(new BasicGame(), 1000, 1000, 40);
+//    }
+//
+//    @Override
+//    public void init() {
+//
+//    }
+//
+//    @Override
+//    public void loop() {
+//        SaxionApp.clear();
+//        map.draw();
+//    }
+//
+//    @Override
+//    public void keyboardEvent(KeyboardEvent keyboardEvent) {
+//
+//    }
+//
+//    @Override
+//    public void mouseEvent(MouseEvent mouseEvent) {
+//
+//    }
+//}
+//
+//
+//
+//
+//
+//
