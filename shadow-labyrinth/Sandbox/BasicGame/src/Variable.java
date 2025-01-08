9c223ab307bfd6d5fc6b73f6553903eb6d4e9cc8
Kian <159238818+KianKamphuis@users.noreply.github.com>
2025-01-08 09:06:01 +0100

    bug fix

--- shadow-labyrinth/Sandbox/BasicGame/src/Variable.java
+++ shadow-labyrinth/Sandbox/BasicGame/src/Variable.java
@@ -2,7 +2,7 @@ import nl.saxion.app.SaxionApp;
 
 public final class Variable {
     public static final int ORIGINAL_TILE_SIZE = 52;
-    public static final int MAX_MAP_COLUMN = 136;
+    public static final int MAX_MAP_COLUMN = 133;
     public static final int MAX_MAP_ROW = 71;
     public static final int SMALL_TILE_SIZE = 16;
     public static final int SCREEN_WIDTH = SaxionApp.getWidth();
