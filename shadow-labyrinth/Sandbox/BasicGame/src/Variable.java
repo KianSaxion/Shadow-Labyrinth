d1c1f63b2f3dec38ce87fd99f95854966725a2a8
Kian <159238818+KianKamphuis@users.noreply.github.com>
2025-01-10 09:54:11 +0100

    changes2

--- shadow-labyrinth/Sandbox/BasicGame/src/Variable.java
+++ shadow-labyrinth/Sandbox/BasicGame/src/Variable.java
@@ -1,7 +1,7 @@
 import nl.saxion.app.SaxionApp;
 
 public final class Variable {
-    public static final int ORIGINAL_TILE_SIZE = 12; //52
+    public static final int ORIGINAL_TILE_SIZE = 52; //52
     public static final int MAX_MAP_COLUMN = 133;
     public static final int MAX_MAP_ROW = 71;
     public static final int SMALL_TILE_SIZE = 16;
